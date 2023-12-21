9d688909d57c61d796504288ad4af79407fc900e
vignesh <vigneshanand17@gmail.com>
2023-12-21 12:52:48 +0530

    Added perspective projection.

--- src/main.c
+++ src/main.c
@@ -10,7 +10,9 @@
 vec3_t cube_points[N_POINTS]; //9x9x9 cube
 vec2_t projected_points[N_POINTS];
 
-float fov_factor = 128;
+vec3_t camera_position = { .x = 0, .y = 0, .z = -5 };
+
+float fov_factor = 640;
 
 int point_count = 0;
 
@@ -56,8 +58,8 @@ void process_input(void)
 // receives a 3d vector and return projected 2d point
 vec2_t project(vec3_t point) {
   vec2_t projected_point = {
-    .x = (fov_factor * point.x),
-    .y = (fov_factor * point.y)
+    .x = (fov_factor * point.x)/ point.z,
+    .y = (fov_factor * point.y) / point.z
   };
   return projected_point;
 }
@@ -65,6 +67,7 @@ vec2_t project(vec3_t point) {
 void update(void) {
   for (int i = 0; i < N_POINTS; i++) {
     vec3_t point = cube_points[i];
+    point.z -= camera_position.z;
     vec2_t projected_point = project(point);
     projected_points[i] = projected_point;
   }
